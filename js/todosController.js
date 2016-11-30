c7d5ceba604aeda8ded24ea16abae78e2b5b5b1d
Aaron Goldsmith <aarongoldsmith@msn.com>
2016-11-30 11:58:57 -0800

    added first todo

--- js/todosController.js
+++ js/todosController.js
@@ -1,17 +1,25 @@
 (function() {
   'use strict';
   angular.module('todosApp')
-  controller('TodosController', TodosController)
+  .controller('TodosController', TodosController)
 
   function TodosController () {
     var self = this;
     self.add = addTodo;
     self.newTodo = { todo: "", image: "", done: false }
+    self.all = [{ todo: "Get paid", image: "xx", done: false }]
 
+    function addTodo () {
+
+
+      self.all.push(self.newTodo)
+      self.newTodo = { todo: "", image: "", done: false }
 
 
     }
 
+
+    }
 
 
 }());
